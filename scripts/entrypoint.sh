3353829552d25f6f9ba1fca226110808c072fc44
Mario Seidel <mario@maniox.de>
2017-01-12 23:51:07 +0100

    fix username for oro install

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,16 +7,17 @@ GROUP_NAME=${HOST_GROUP:-group}
 
 echo "Starting with UID: $USER_ID and GID: $GROUP_ID"
 groupadd -g $GROUP_ID -o $GROUP_NAME \
-	&& useradd --shell /bin/bash -u $USER_ID -o -c "" -g $GROUP_ID -m $USER_NAME
-export HOME=/home/$USER_NAME
+	&& useradd --shell /bin/bash -u $USER_ID -o -c "" -g $GROUP_ID -m $HOST_USER
+export HOME=/home/$HOST_USER
 
 echo "run composer install/update"
 if [ -e composer.lock ]; then
 	php -d allow_url_fopen=on /usr/local/bin/composer.phar self-update \
-		&&	su -c 'php -d allow_url_fopen=on /usr/local/bin/composer.phar install --prefer-source' $USER_NAME
+		&&	su -c 'php -d allow_url_fopen=on /usr/local/bin/composer.phar install --prefer-source' $HOST_USER
 else
+    su -c 'php -d allow_url_fopen=on /usr/local/bin/composer.phar global require fxp/composer-asset-plugin:1.2.2' $HOST_USER
 	php -d allow_url_fopen=on /usr/local/bin/composer.phar self-update \
-		&&	su -c 'php -d allow_url_fopen=on /usr/local/bin/composer.phar update --prefer-stable --prefer-source' $USER_NAME
+		&&	su -c 'php -d allow_url_fopen=on /usr/local/bin/composer.phar update --prefer-stable --prefer-source' $HOST_USER
 fi
 
 echo -e "============= starting webserver with ENV $APP_ENV ============="
